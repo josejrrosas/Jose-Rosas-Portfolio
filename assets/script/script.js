1d3f74d631e718a5f9d24bdb9dffeea1d4cad16f
Jose Rosas <josejrrosas@yahoo.com>
2021-06-27 21:20:39 -0500

    portfolio style complete

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,10 @@
 const hamburgerButton = document.getElementById('hamburger');
 const navList = document.getElementById("nav_links");
+const nameDiv = document.getElementById("name");
 
 function toggleButton() {
     navList.classList.toggle('show');
-
+    nameDiv.classList.toggle('hide');
 }
 
 hamburgerButton.addEventListener('click', toggleButton);
