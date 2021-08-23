b53eff21b4837ffc2737496f34b68d4ac9c1a052
Jose Rosas <josejrrosas@yahoo.com>
2021-08-22 22:21:50 -0500

    fully redone portfolio

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,10 +1,8 @@
-const hamburgerButton = document.getElementById('hamburger');
-const navList = document.getElementById("nav_links");
-const nameDiv = document.getElementById("name");
+const hamburgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
 
 function toggleButton() {
-    navList.classList.toggle('show');
-    nameDiv.classList.toggle('hide');
+    navList.classList.toggle('show')
 }
 
-hamburgerButton.addEventListener('click', toggleButton);
+hamburgerButton.addEventListener('click', toggleButton)
